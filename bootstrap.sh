6435a98e66bf527c46831f90aa815a76c0c76b44
sohnemann <50672942+sohnemann@users.noreply.github.com>
2024-10-09 21:24:42 +1300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ apt-get install -y chef git build-essential
 systemctl stop chef-client
 systemctl mask chef-client
 if [ ! -d "/chef_bootstrap" ]; then
-	git clone https://github.com/palfrey/raspberry-chef /chef_bootstrap
+	git clone https://github.com/sohnemann/raspberry-chef /chef_bootstrap
 fi
 cd /chef_bootstrap/config
 rm -Rf local-mode-cache # Won't exist on first run, but for later ones this avoids caching
